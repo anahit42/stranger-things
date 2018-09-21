d5670b979fe14b6d67d7f8922b5b79bc733c455b
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-09-21 20:11:32 +0400

    config: add config variables.

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  MONGODB: {
+    CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING
+  },
+  TWITTER: {
+    ACCESS_TOKEN: process.env.TWITTER_ACCESS_TOKEN,
+    ACCESS_TOKEN_SECRET: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    API_KEY: process.env.TWITTER_API_KEY,
+    API_SECRET: process.env.TWITTER_API_SECRET
+  }
+}
